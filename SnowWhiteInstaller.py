f233886ce6e34c5efa517ac46490ae9f8d1f0a6d
Joseph Bollman <joseph.bollman@itaas.dimensiondata.com>
2017-09-15 23:15:26 -0700

    Fixed bugs in autostart, screensaver, and screen rotation

--- SnowWhiteInstaller.py
+++ SnowWhiteInstaller.py
@@ -2,8 +2,8 @@ import os
 import fileinput
 import subprocess
 import re
-#ScreenOrientation = 'Landscape'
-ScreenOrientation = 'Portrait'
+ScreenOrientation = 'Landscape'
+#ScreenOrientation = 'Portrait'
 
 if os.path.exists('SnowWhite.py'):
     FullFileName = os.path.abspath('SnowWhite.py')
@@ -27,34 +27,47 @@ if os.path.exists('SnowWhite.py'):
             print('Successfully installed python-forecastio module')
         except:
             raise Exception("Failed to install 'python-forecastio' module")
-
-    print('Attempting to make SnowWhite.py auto-start at boot up...')
-    try:
-        os.system("echo '@sudo /usr/bin/python " + FullFileName + "' >> ~/.config/lxsession/LXDE-pi/autostart")
+    AutoStartCheck = subprocess.Popen(['grep','@/usr/bin/python ' + FullFileName, '/home/pi/.config/lxsession/LXDE-pi/autostart'], stdout=subprocess.PIPE).communicate()[0]
+    if not re.search('@/usr/bin/python', AutoStartCheck):
+        os.system("echo '@/usr/bin/python " + FullFileName + "' >> /home/pi/.config/lxsession/LXDE-pi/autostart")
         os.system("sudo chmod +x " + FullFileName)
-        #modify /boot/config.txt add display_rotate=3
-    except:
-            raise Exception("Failed to make SnowWhite.py auto-start at boot up")
-    print('Successfully made SnowWhite.py auto-start at boot up')
+        print('Snow White Application has been set to Auto Start')
+    else:
+        print('Snow White Application has already been set to Auto Start')
+    ScreenSaverCheck = subprocess.Popen(['grep','xserver-command=X -s 0 -dpms', '/etc/lightdm/lightdm.conf'], stdout=subprocess.PIPE).communicate()[0]
+    if not re.search('xserver-command=X -s 0 -dpms', ScreenSaverCheck):
+        os.system('sudo echo "xserver-command=X -s 0 -dpms" >> /etc/lightdm/lightdm.conf')
+        print('Successfully turned off screen saver')
+    else:
+        print('Screen saver already turned off')
+
+    if ScreenOrientation == 'Portrait':
+        ScreenOrientationCheck = subprocess.Popen(['grep','display_rotate', '/boot/config.txt'], stdout=subprocess.PIPE).communicate()[0]
+        if re.search('display_rotate', ScreenOrientationCheck):
+            for line in fileinput.input('/boot/config.txt', inplace = True):
+                if re.search('display_rotate', line):
+                    DisplayRotateLine = (re.search('display_rotate', line)).string
+                    print(line.replace(DisplayRotateLine, "display_rotate=3"))
+                else:
+                    print(line,)
+            print("Portrait display has been set.")
+        else:
+            os.system('sudo echo "display_rotate=3" >> /boot/config.txt')
+            print("Portrait display has been set.")        
+    else:
+        ScreenOrientationCheck = subprocess.Popen(['grep','display_rotate', '/boot/config.txt'], stdout=subprocess.PIPE).communicate()[0]
+        if re.search('display_rotate', ScreenOrientationCheck):
+            for line in fileinput.input('/boot/config.txt', inplace = True):
+                if re.search('display_rotate', line):
+                    DisplayRotateLine = (re.search('display_rotate', line)).string
+                    print(line.replace(DisplayRotateLine, "display_rotate=0"))
+                else:
+                    print(line,)
+            print("Landscape display has been set.")
+        else:
+            os.system('sudo echo "display_rotate=0" >> /boot/config.txt')
+            print("Landscape display has been set.")        
+        
 else:
     raise Exception("Cannot find SnowWhite.py file")
 
-ScreenSaverCheck = subprocess.Popen(['grep','xserver-command=X -s 0 -dpms', '/etc/lightdm/lightdm.conf'], stdout=subprocess.PIPE).communicate()[0]
-if not re.search('xserver-command=X -s 0 -dpms', ScreenSaverCheck):
-    os.system('sudo echo "xserver-command=X -s 0 -dpms" >> /etc/lightdm/lightdm.conf')
-    print('Successfully turned off screen saver')
-else:
-    print('Screen saver already turned off')
-
-if ScreenOrientation == 'Portrait':
-    ScreenOrientationCheck = subprocess.Popen(['grep','display_rotate', '/boot/config.txt'], stdout=subprocess.PIPE).communicate()[0]
-    if re.search('display_rotate', ScreenOrientationCheck):
-        for line in fileinput.input('/boot/config.txt', inplace = True):
-            if re.search('display_rotate', line):
-                DisplayRotateLine = (re.search('display_rotate', line)).string
-                print line.replace(DisplayRotateLine, "display_rotate=3")
-            else:
-                print line
-    else:
-        os.system('sudo echo "display_rotate=3" >> /boot/config.txt')
-    print("Portrait display has been set.")
